029c2a1869fbc3e696605eff2052a7d1ededf939
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-07-30 16:12:52 +0000

    Tue, Jul 30, 2024, 10:12 AM -06:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,9 +1,9 @@
 file_name = input("File name: ")
 
-def direction():
-    print("image/")
-
-
 if file_name.endswith(".gif"):
-    return 
+    file_type = "gif"
 
+elif file_name.endswith(".jpg"):
+    file_type = "jpg"
+
+print("image/" + file_type)
