c6b933e085223862579487bbb46700ea8c7449fa
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-07-30 16:46:15 +0000

    Tue, Jul 30, 2024, 10:46 AM -06:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -4,7 +4,7 @@ applications = [".pdf", ".zip"]
 text_app = "txt"
 
 if file_name.endswith(".gif")or(".png")or(".jpg")or(".jpeg"):
-    print("image/" + file_name.endswith())
+    print("image/" + file_name)
 
 else:
     print("application/octet-stream")
