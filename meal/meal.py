3e98700e81493e713d50de4eecd02de49f75fbb9
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-07-30 20:37:35 +0000

    Tue, Jul 30, 2024, 2:37 PM -06:00

--- meal/meal.py
+++ meal/meal.py
@@ -12,11 +12,13 @@ def main():
     # 8:30
     time = convert(user_input)
     # print(time)
-    if 7.0 <= time < 8.0:
-        print("breakfast time")
-    elif 12.0 <= time < 13.0:
-        print("lunch time")
-    elif 18.0 <= time < 19.0:
-        print("dinner time")
+    return
+
+if 7.0 <= time < 8.0:
+    print("breakfast time")
+elif 12.0 <= time < 13.0:
+    print("lunch time")
+elif 18.0 <= time < 19.0:
+    print("dinner time")
 
 main()
