27a6729d0453791c35fba470e87371f22f251a78
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-07-30 18:38:01 +0000

    Tue, Jul 30, 2024, 12:38 PM -06:00

--- meal/meal.py
+++ meal/meal.py
@@ -4,7 +4,6 @@ def convert(time):
     hours, minutes = time.split(":")
     minutes = float(minutes) * 100.0 / 60.0
     hours = float(hours)
-    global time
     time = hours + minutes
 
 if 7.0 <= time < 9.0:
