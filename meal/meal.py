115dce309371e541dad33f27e973e914fd030215
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-07-30 18:29:03 +0000

    Tue, Jul 30, 2024, 12:29 PM -06:00

--- meal/meal.py
+++ meal/meal.py
@@ -2,7 +2,7 @@ time = input("What time is it? ")
 
 def convert(time):
     hours, minutes = time.split(":")
-    
+    minutes = int(minutes) * 100
 
 if 7 <= hours < 9:
     print("breakfast time")
