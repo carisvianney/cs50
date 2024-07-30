2e123955e4f4e36a2c34e82c6e0ad906d8296472
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-07-30 14:40:03 +0000

    Tue, Jul 30, 2024, 2:40 PM +00:00

---
+++ deep/deep.py
@@ -0,0 +1,7 @@
+answer = input("What is the answer to the Great Question of Life, the Universe, and Everything? ")
+
+if answer == "42" or "forty-two" or "forty two":
+    print("Yes")
+
+else:
+    print("No")
