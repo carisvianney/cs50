17fa6c8f2a0bafe9ba885bc4a25a49128872317d
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-07-30 21:00:40 +0000

    Tue, Jul 30, 2024, 3:00 PM -06:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,13 +1,5 @@
 user_input = input("What time is it? ")
 
-def convert(time):
-    hours, minutes = time.split(":")
-    minutes = float(minutes) / 60.0
-    hours = float(hours) + minutes
-    # print(hours, minutes)
-    time = hours
-    return time
-
 def main():
     # 8:30
     time = convert(user_input)
@@ -19,4 +11,13 @@ def main():
     elif 18.0 <= time < 19.0:
         print("dinner time")
 
-main()
+def convert(time):
+    hours, minutes = time.split(":")
+    minutes = float(minutes) / 60.0
+    hours = float(hours) + minutes
+    # print(hours, minutes)
+    time = hours
+    return time
+
+if __name__ == "__main__":
+    main()
