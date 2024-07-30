77c8a2f047e8c5f4d3fee55f3221e733d3ad2110
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-07-30 16:41:42 +0000

    Tue, Jul 30, 2024, 10:41 AM -06:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -3,7 +3,7 @@ images = [".gif", ".png", ".jpg", ".jpeg"]
 applications = [".pdf", ".zip"]
 text_app = "txt"
 
-if file_name.endswith in images:
+if file_name.endswith(".gif", ".png", ".jpg", ".jpeg"):
     print("image/" + file_name.endswith(3))
 
 else:
