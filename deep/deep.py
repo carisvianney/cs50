f8d1a85e212ede5c37f477f376a305e225cbdc48
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-07-30 14:45:50 +0000

    Tue, Jul 30, 2024, 2:45 PM +00:00

--- deep/deep.py
+++ deep/deep.py
@@ -3,5 +3,5 @@ answer = input("What is the answer to the Great Question of Life, the Universe, 
 if answer == "42" or "forty-two" or "forty two":
     print("Yes")
 
-else:
+elif answer != "42" or "forty-two" or "forty two":
     print("No")
