715e90c8c6a7de261c0871a1d8eed37fb552613b
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-07-30 17:33:00 +0000

    Tue, Jul 30, 2024, 11:33 AM -06:00

---
+++ interpreter.py
@@ -0,0 +1,16 @@
+x, y, z = input("Expression: ")
+x = float(x)
+z = float(z)
+
+if y == "+":
+    print(x + z)
+
+elif y == "-":
+    print(x - z)
+
+elif y == "*":
+    print(x * z)
+
+elif y == "/":
+    print(x / z)
+
