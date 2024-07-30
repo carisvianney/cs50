89625f5b7d5d74e6dbc87a4b30ae5a54e9357d19
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-07-30 15:43:34 +0000

    Tue, Jul 30, 2024, 9:43 AM -06:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,6 +1,6 @@
-Greeting = input("Greeting: ")
+Greeting = str.lower(input("Greeting: "))
 
-if Greeting.lower == "hello":
+if Greeting == "hello":
     print("$0")
 
 elif Greeting.startswith == "h":
