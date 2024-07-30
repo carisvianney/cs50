af2e29028deff42da2b19bf9d7e17d03672a334c
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-07-30 18:57:16 +0000

    Tue, Jul 30, 2024, 12:57 PM -06:00

--- meal/meal.py
+++ meal/meal.py
@@ -2,9 +2,10 @@ time = input("What time is it? ")
 
 def convert(time):
     hours, minutes = time.split(":")
-    hours = float(hours)
     minutes = float(minutes) * 100.0 / 60.0
+    hours = float(hours) + minutes
 
-main()
+def main()
+    
 
 print(time)
