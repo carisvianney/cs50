49e160095ed746d03a6aa3f66cb78fd30c2d401c
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-08-14 18:00:47 +0000

    Wed, Aug 14, 2024, 12:00 PM -06:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -3,8 +3,10 @@ Input = input("Input: ")
 def main():
     vowels = ["a", "e", "i", "o", "u"]
     for letter in Input:
-        while letter not in vowels:
+        if letter not in vowels:
             print(letter, end="")
+        else:
+            print("", end="")
 
 
 main()
