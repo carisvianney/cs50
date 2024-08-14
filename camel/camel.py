cb0e59d3571e071273e1243bf061f071271cccf9
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-08-14 01:14:23 +0000

    Tue, Aug 13, 2024, 7:14 PM -06:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,8 +1,10 @@
 camelCase = input("camelCase: ")
 
-def snake_case(camelCase):
-    # Inserta un "_" antes de una letra mayúscula y conviértela a minúscula
-    str.upper in camelCase
+str.upper in camelCase
     return
 
-snake_case(camelCase)
+#def snake_case(camelCase):
+    # Inserta un "_" antes de una letra mayúscula y conviértela a minúscula
+
+
+#snake_case(camelCase)
