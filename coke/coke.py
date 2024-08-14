ad156dc15b68d9317182a10d5c536cac52f52992
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-08-14 08:22:38 +0000

    Wed, Aug 14, 2024, 2:22 AM -06:00

--- coke/coke.py
+++ coke/coke.py
@@ -2,17 +2,16 @@ amount_due = 50
 coins_permitted = [25, 10, 5]
 
 #informar monto total
-print("Amount due: " + str(amount_due))
+#print("Amount due: " + str(amount_due))
 
 #insertar moneda loop
 while amount_due > 0:
+    print("Amount due: ", end="")
+    print(amount_due)
     coin_inserted = int(input("Insert coin: "))
     if coin_inserted in coins_permitted:
             amount_due = amount_due - coin_inserted
-            if amount_due > 0:
-                print("Amount due: ", end="")
-                print(amount_due)
-            
+
 
 else:
     print("Change Owed: ", end="")
