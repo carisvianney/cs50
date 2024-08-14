3b9b738ca177d8d457e1854aef1fcac9ff2461fb
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-08-14 04:32:49 +0000

    Tue, Aug 13, 2024, 10:32 PM -06:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,15 +1,8 @@
 camelCase = input("camelCase: ")
 
-#print str
-#for letter in list(camelCase):
-    #print(letter, end="")
 
-def find_upper(list):
-    for letter in list:
-        if letter.isupper():
-            
+for letter in list(camelCase):
+    if letter.isupper():
+        letter = letter.lower()
+    print(letter, end="")
 
-
-#find upperclass
-
-#add _ before upperclass
