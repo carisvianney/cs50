55e2cfaa48d4a27fc36adccd0b31941ae9ee3574
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-08-14 07:58:56 +0000

    Wed, Aug 14, 2024, 1:58 AM -06:00

--- coke/coke.py
+++ coke/coke.py
@@ -7,9 +7,10 @@ print("Amount due: " + str(amount_due))
 #insertar moneda loop
 while amount_due > 0:
     coin_inserted = int(input("Insert coin: "))
-    amount_due = amount_due - coin_inserted
-    print("Amount due: ", end="")
-    print(amount_due)
+    if coin_inserted in coins_permitted:
+        amount_due = amount_due - coin_inserted
+        print("Amount due: ", end="")
+        print(amount_due)
 
 """
 #restar moneda incluida en la lista al monto total
