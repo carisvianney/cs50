8b472b3afb3f944fa80312872387d3bdcb1c9fa4
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-08-14 17:33:01 +0000

    Wed, Aug 14, 2024, 11:33 AM -06:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -3,7 +3,7 @@ Input = input("Input: ")
 def main():
     vowels = ["a", "e", "i", "o", "u"]
     for letter in Input:
-        if letter in vowels:
+        if letter.lower in vowels:
             print("", end="")
         else:
             print(letter, end="")
