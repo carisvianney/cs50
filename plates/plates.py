6c18d0ba8c27716b823c25481d9f055cabf6c365
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-08-14 21:55:20 +0000

    Wed, Aug 14, 2024, 3:55 PM -06:00

--- plates/plates.py
+++ plates/plates.py
@@ -9,15 +9,18 @@ def main():
 def is_valid(plate):
     if plate[0].isalpha() and plate[1].isalpha():
         if len(plate) <= 6 and len(plate) >= 2:
-            if num_valid(plate):
+          #  if num_valid(plate):
                 return True
             else:
                 False
 
+"""
+    “Numbers cannot be used in the middle of a plate; they must come at the end.
+    For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable.
+    The first number used cannot be a ‘0’.”
+"""
+
 def num_valid(plate):
-    # “Numbers cannot be used in the middle of a plate; they must come at the end.
-    # For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable.
-    # The first number used cannot be a ‘0’.”
     n = len(plate)
     middle_characters = plate[2:n]
     if middle_characters[0] == 0:
