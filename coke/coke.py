f1519e29c2efadfca8119d79c8f00a536f59a58c
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-08-14 06:11:33 +0000

    Wed, Aug 14, 2024, 12:11 AM -06:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,7 +1,8 @@
-def main():
-    coke = 50
-    coins_permitted = [25, 10, 5]
-    print("Amount due: " + str(coke))
+amount_due = 50
+coins_permitted = [25, 10, 5]
+
+print("Amount due:", end="")
+print(amount_due)
 
 coin_inserted = int(input("Insert coin: "))
 
