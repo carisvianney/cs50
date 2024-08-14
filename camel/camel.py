6750c878a9ab8588b23e6f400eeb3f0630533ad5
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-08-14 01:38:16 +0000

    Tue, Aug 13, 2024, 7:38 PM -06:00

--- camel/camel.py
+++ camel/camel.py
@@ -3,7 +3,9 @@ camel_case = input("camelCase: ")
 def find_upper(cadena):
     for letter in cadena:
         if letter.isupper():
-            return letter
+            return "_" + letter
     return None
 
+upper_letter = find_upper(camel_case)
 
+print(upper_letter)
