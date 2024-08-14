7ec640199aee3f7e7a5d5585032673a1a4a4c561
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-08-14 06:31:17 +0000

    Wed, Aug 14, 2024, 12:31 AM -06:00

--- coke/coke.py
+++ coke/coke.py
@@ -8,7 +8,7 @@ coin_inserted = int(input("Insert coin: "))
 
 def insert_coin(coin_inserted):
     while coin_inserted in coins_permitted:
-        amount_due = amount_due - coin_inserted
+        amount_due = 50 - coin_inserted
         print("Amount due:", end="")
         print(amount_due)
 
