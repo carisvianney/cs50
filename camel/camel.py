36cb0d2b4aa2911b54cdd2a4e06b41c98e9dd8af
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-08-14 01:31:09 +0000

    Tue, Aug 13, 2024, 7:31 PM -06:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,11 +1,6 @@
 camel_case = input("camelCase: ")
 
 def find_upper(cadena):
-    
-
-
-#def camel_to_snake_case(camelCase):
-    # Inserta un "_" antes de una letra mayúscula y conviértela a minúscula
-
-
-#snake_case(camelCase)
+    for letter in cadena:
+        if letter.isupper()
+        return "_" + letter
