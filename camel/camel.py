40984c4aae3713007704a113f82126fb71c2229f
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-08-14 04:12:23 +0000

    Tue, Aug 13, 2024, 10:12 PM -06:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,9 +1,9 @@
 camelCase = input("camelCase: ")
 
 #print str
-for letter in camelCase:
-    print(letter, end="")
-    
+for letter in list(camelCase):
+    print(letter, )
+
 #find upperclass
 
 #add _ before upperclass
