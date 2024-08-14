cb76916f959f4d044622ebc24cd847ba47e9f7ca
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-08-14 06:42:22 +0000

    Wed, Aug 14, 2024, 12:42 AM -06:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,3 +1,11 @@
+def main():
+    while True:
+        for coin_inserted in coins_permitted:
+            amount_due = amount_due - coin_inserted
+        print("Amount due:", end="")
+        print(amount_due)
+        break
+
 amount_due = 50
 coins_permitted = [25, 10, 5]
 
@@ -5,6 +13,8 @@ print("Amount due:", end="")
 print(amount_due)
 
 coin_inserted = int(input("Insert coin: "))
+
+
 """
 def insert_coin(coin_inserted):
     while coin_inserted in coins_permitted:
@@ -16,14 +26,5 @@ def insert_coin(coin_inserted):
 for coin_inserted
 insert_coin(coin_inserted)
 """
-def main():
-    while True:
-        for coin_inserted in coins_permitted:
-            amount_due = amount_due - coin_inserted
-        print("Amount due:", end="")
-        print(amount_due)
-        break
-
-coin_inserted = int(input("Insert coin: "))
 
 main()
