80bd68029b7aa55da04977549bad2195a1283538
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-08-14 20:41:21 +0000

    Wed, Aug 14, 2024, 2:41 PM -06:00

--- plates/plates.py
+++ plates/plates.py
@@ -9,11 +9,11 @@ def main():
 def is_valid(plate):
     if plate[0].isalpha() and plate[1].isalpha():
         if len(plate) <= 6 and len(plate) >= 2:
-            if 
-                return True
-            else:
-                False
+            return True
+        else:
+            False
 
-
+def num_valid(plate):
+    
 
 main()
