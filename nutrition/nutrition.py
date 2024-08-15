f6e60dd2e86339edf0867d1e9355a185d803c063
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-08-15 07:12:08 +0000

    Thu, Aug 15, 2024, 1:12 AM -06:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,30 +1,30 @@
-fruits = [
-    {"Item": "Apple", "calories": "130"},
-    {"Item": "Avocado", "calories": "50"},
-    {"Item": "Banana", "calories": "110"},
-    {"Item": "Cantaloupe", "calories": "50"},
-    {"Item": "Grapefruit", "calories": "60"},
-    {"Item": "Grapes", "calories": "90"},
-    {"Item": "Honeydew", "calories": "50"},
-    {"Item": "Kiwifruit", "calories": "90"},
-    {"Item": "Lemon", "calories": "15"},
-    {"Item": "Lime", "calories": "20"},
-    {"Item": "Nectarine", "calories": "60"},
-    {"Item": "Orange", "calories": "80"},
-    {"Item": "Peach", "calories": "60"},
-    {"Item": "Pear", "calories": "100"},
-    {"Item": "Pineapple", "calories": "50"},
-    {"Item": "Plums", "calories": "70"},
-    {"Item": "Strawberries", "calories": "50"},
-    {"Item": "Sweet Cherries", "calories": "100"},
-    {"Item": "Tangerine", "calories": "50"},
-    {"Item": "Watermelon", "calories": "80"}
-]
+fruits = {
+    "Apple": "130",
+    "Avocado": "50",
+    "Banana": "110",
+    "Cantaloupe": "50",
+    "Grapefruit": "60",
+    "Grapes": "90",
+    "Honeydew": "50",
+    "Kiwifruit": "90",
+    "Lemon": "15",
+    "Lime": "20",
+    "Nectarine": "60",
+    "Orange": "80",
+    "Peach": "60",
+    "Pear": "100",
+    "Pineapple": "50",
+    "Plums": "70",
+    "Strawberries": "50",
+    "Sweet Cherries": "100",
+    "Tangerine": "50",
+    "Watermelon": "80"
+}
 
 fruit = input("Item: ")
 
 if fruit in fruits:
-    print("Calories: " + fruit["calories"])
+    print("Calories: " + fruit)
 else:
     print("Not in fruits")
 
