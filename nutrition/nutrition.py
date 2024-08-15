c7fc21c29c2bab20ec7fd14e9bebdd117493e6b7
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-08-15 06:56:42 +0000

    Thu, Aug 15, 2024, 12:56 AM -06:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,23 +1,23 @@
 fruits = [
     {"Item": "Apple", "calories": "130"},
     {"Item": "Avocado", "calories": "50"},
-    {"Item": "Banana", "calories": "110"}
-    {"Item": "Cantaloupe", "calories": "50"}
-    {"Item": "Grapefruit", "calories": "60"}
-    {"Item": "Grapes", "calories": "90"}
-    {"Item": "Honeydew", "calories": "50"}
-    {"Item": "Kiwifruit", "calories": "90"}
-    {"Item": "Lemon", "calories": "15"}
-    {"Item": "Lime", "calories": "20"}
-    {"Item": "Nectarine", "calories": "60"}
-    {"Item": "Orange", "calories": "80"}
-    {"Item": "Peach", "calories": "60"}
-    {"Item": "Pear", "calories": "100"}
-    {"Item": "Pineapple", "calories": "50"}
-    {"Item": "Plums", "calories": "70"}
-    {"Item": "Strawberries", "calories": "50"}
-    {"Item": "Sweet Cherries", "calories": "100"}
-    {"Item": "Tangerine", "calories": "50"}
+    {"Item": "Banana", "calories": "110"},
+    {"Item": "Cantaloupe", "calories": "50"},
+    {"Item": "Grapefruit", "calories": "60"},
+    {"Item": "Grapes", "calories": "90"},
+    {"Item": "Honeydew", "calories": "50"},
+    {"Item": "Kiwifruit", "calories": "90"},
+    {"Item": "Lemon", "calories": "15"},
+    {"Item": "Lime", "calories": "20"},
+    {"Item": "Nectarine", "calories": "60"},
+    {"Item": "Orange", "calories": "80"},
+    {"Item": "Peach", "calories": "60"},
+    {"Item": "Pear", "calories": "100"},
+    {"Item": "Pineapple", "calories": "50"},
+    {"Item": "Plums", "calories": "70"},
+    {"Item": "Strawberries", "calories": "50"},
+    {"Item": "Sweet Cherries", "calories": "100"},
+    {"Item": "Tangerine", "calories": "50"},
     {"Item": "Watermelon", "calories": "80"}
 ]
 
