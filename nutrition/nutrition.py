29003ba2fe9a4c7f141c42ba974dd5df38daeffb
Vianney Rivera <136045348+carisvianney@users.noreply.github.com>
2024-08-15 06:29:44 +0000

    Thu, Aug 15, 2024, 12:29 AM -06:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,5 +1,9 @@
 fruits = {
     "Item": "Apple", "calories": "130"
     "Item": "Avocado", "calories": "50"
-    "Item": "Banana", "calories": "
+    "Item": "Banana", "calories": "110"
+    "Item": "Cantaloupe", "calories": "50"
+    "Item": "Grapefruit", "calories": "60"
+    "Item": "Grapes", "calories": "90"
+    
 }
