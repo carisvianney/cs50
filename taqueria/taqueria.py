145d0ccf07645b100c84bb6f2c16526e8b5f81e5
Vianney Rivera <carisvrp@gmail.com>
2024-08-20 23:26:03 -0600

    Sun, Aug 19, 2024

---
+++ taqueria/taqueria.py
@@ -0,0 +1,32 @@
+menu = {
+
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+
+}
+
+def main():
+    # order_placed = False
+    total = 0
+    while True:
+        try:
+            item = input("Item: ")
+            item = item.title()
+            if item in menu:
+                total += menu.get(item)
+                print("Total: $" + str(f"{total:.2f}"))
+            else:
+                pass
+        except EOFError:
+            # order_placed = True
+            print("")
+            break
+
+main()
