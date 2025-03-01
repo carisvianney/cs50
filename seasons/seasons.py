f4bc22ad0aaaa2e165b8a6e998d417dda4273db8
Vianney Rivera <carisvrp@gmail.com>
2025-02-28 23:45:42 -0600

    Problem set 8.1

---
+++ seasons/seasons.py
@@ -0,0 +1,35 @@
+from datetime import date, timedelta
+import re
+import operator
+import inflect
+p = inflect.engine()
+
+
+def main():
+    ...
+    minutes = get_minutes(input("Date of Birth: "))
+    if minutes:
+        print(transcript(minutes) + " minutes")
+        return
+    print("Invalid")
+
+
+def get_minutes(s):
+    ...
+    if match := re.search(r"^(\d{4})\-(\d{2})\-(\d{2})$", s):
+        Date = date(int(match.group(1)), int(match.group(2)), int(match.group(3)))
+        today = date.today()
+        duration = operator.__sub__(today, Date)
+        minutes = timedelta.total_seconds(duration)/60
+
+        return minutes
+    return False 
+
+
+def transcript(s):
+    ...
+    return p.number_to_words(s)
+
+
+if __name__ == "__main__":
+    main()
